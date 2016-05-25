1aaa9eff582bb1281eba3f2e4223e8f6c505cf46
Scott Turnbull <streamweaver@gmail.com>
2016-05-24 23:24:17 -0400

    Added new new field to weapons and generated new weapon data

--- scripts/csv_data.py
+++ scripts/csv_data.py
@@ -1,5 +1,4 @@
-import csv
-from django.utils.text import slugify
+import csv, argparse, string
 
 def parse_skills():
     """
@@ -41,38 +40,75 @@ def parse_weapons():
     with open('Weapons.csv', 'rb') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            aliases = ""
-            if row['aliases']:
-                aliases = ", ".join("'%s'" % a.strip() for a in row['aliases'].split(','))
+            modify_weapon(row)
             output += """
     {} = {{
         'name': '{}',
-        'aliases': [{}],
+        'aliases': {},
         'typeclass': '{}',
         'desc': '{}',
         'weight': {},
         'value': {},
         'damage': {},
-        'ware': {},
+        'wear': {},
+        'durability': {},
         'bonus': {},
         'use_skill': '{}',
         'fix_skill': '{}',
             }}
             """.format(
-                row['key'].upper(),
-                row['name'].title(),
-                aliases,
+                row['key'],
+                row['name'],
+                row['aliases'],
                 row['typeclass'],
                 row['desc'],
                 row['weight'],
                 row['value'],
                 row['damage'],
-                row['ware'],
+                row['wear'],
+                row['durability'],
                 row['bonus'],
                 row['use_skill'],
                 row['fix_skill']
             )
         print(output)
 
+def modify_weapon(row):
+    """
+    Does some modification of weapon names and aliases to spice them up.
+
+    :param row: dict of weapon data
+    """
+    # turn aliases into a list.
+    aliases = [] if not row['aliases'] else row['aliases'].split(',')
+
+    # Put some jazz on the names.
+    if row['model']:
+        model  = "%s%s" % (row["model"].upper(), row["damage"])
+        if int(row['bonus']) > 0:
+            model = "%s%s" % (model, string.ascii_lowercase[int(row["bonus"])])
+        row['name'] = "%s %s" % (model, row['name'].title())
+        aliases.append(model.lower())
+
+    row['aliases'] = aliases
+    row['key'] = row['name'].replace(" ", "_").lower().upper()
+
+
 if __name__ == "__main__":
-    parse_weapons()
+    parse_functions = {
+        "skills": parse_skills,
+        "weapons": parse_weapons,
+    }
+    parser = argparse.ArgumentParser()
+    type_options = "|".join([k for k in parse_functions.keys()])
+    help_text = "Type of csv to parse. [%s]" % type_options
+    parser.add_argument("type",
+                        help=help_text)
+    args = parser.parse_args()
+
+    if args.type in parse_functions:
+        func = parse_functions[args.type]
+        func()
+    else:
+        print("'%s' is not a valid option.\n" % args.type)
+        print(help_text)
